d8128d6f5bdc47abda8da144efa539378acc8c45
e <15602261894@163.com>
2020-04-10 00:20:22 +0800

    知乎列表

---
+++ MyZhiHuDaily/components/newList/newList.js
@@ -0,0 +1,26 @@
+// components/newList/newList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    data: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
